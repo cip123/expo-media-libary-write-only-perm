ad5f455451eccdcdc0200cc2643668cc796a0894
cip <ciprian.trusca@gmail.com>
2023-07-19 20:36:54 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
+
+    const saveToMediaLibrary = async () => {
+        const writeOnlyPermissions = await MediaLibrary.requestPermissionsAsync(true);
+        console.log(writeOnlyPermissions);
+
+        if (writeOnlyPermissions.granted) {
+            const imageUri ="https://baconmockup.com/360/360/"
+            const asset = await MediaLibrary.createAssetAsync(imageUri);
+            console.log(asset)
+        }
+
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title={"TEST"} onPress={() => saveToMediaLibrary()}>
+      </Button>
+
+
       <StatusBar style="auto" />
     </View>
   );
